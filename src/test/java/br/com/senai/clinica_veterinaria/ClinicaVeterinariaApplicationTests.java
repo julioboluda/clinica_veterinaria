ac0c10ab68e2564c58bfeeef3bc9d02888fe0980
julioboluda <julioboluda@gmail.com>
2026-02-24 10:59:30 -0300

    primeiro commit

---
+++ src/test/java/br/com/senai/clinica_veterinaria/ClinicaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.clinica_veterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
