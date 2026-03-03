12df2d99df1af0854e534bdbd8ef1034e5df0a23
julioboluda <julioboluda@gmail.com>
2026-03-03 09:26:14 -0300

    03-03-2026/ClinicaVeterinaria

--- src/main/java/br/com/senai/clinica_veterinaria/entity/Endereco.java
+++ src/main/java/br/com/senai/clinica_veterinaria/entity/Endereco.java
@@ -1,6 +1,6 @@
 package br.com.senai.clinica_veterinaria.entity;
 
-import java.util.List;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -8,7 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
